5608019cf4f3cd19f3f06c76d6c52ea9ce68a0b5
SpartanTech <SpartanTech@users.noreply.github.com>
2021-04-15 21:04:03 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'python-miio-s7',
+    packages = ['python-miio'],
+    version = '5.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'S7 Commit',
+    author = '',
+    author_email = '',
+    url = 'https://github.com/SpartanTech/python-miio',
+    download_url = 'download link you saved',
+    keywords = ['tag1', 'tag2'],
+    classifiers = [],
+)
